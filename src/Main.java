6b4c06da5d66860f64cf53ce614f091ae8c63772
Tam <enquiretam@gmail.com>
2024-04-16 11:32:24 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int userNumber;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hey! Enter a number between 1-100");
+
+        int randomNumber = (int) (Math.random() * 100) + 1; // Generating random number outside the loop
+
+        while (true) {
+            userNumber = scanner.nextInt();
+            if (userNumber < 1 || userNumber > 100) {
+                System.out.println("** Please try again **");
+            } else if (userNumber > randomNumber) {
+                System.out.println("Too big... guess again...");
+            } else if (userNumber < randomNumber) {
+                System.out.println("Too small... guess again...");
+            } else {
+                System.out.println("Great Guess! Well done!");
+                break;
+            }
         }
     }
 }

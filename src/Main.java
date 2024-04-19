1a5d3291927ac74303b039e3d862ccaebc0efaa5
Tam <enquiretam@gmail.com>
2024-04-19 09:44:20 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,40 @@
+
+
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        int userNumber;
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Hey! Enter a number between 1-100");
+//
+//        int randomNumber = (int) (Math.random() * 100) + 1; // Generating random number outside the loop
+//
+//        while (true) {
+//            userNumber = scanner.nextInt();
+//            if (userNumber < 1 || userNumber > 100) {
+//                System.out.println("** Please try again **");
+//            } else if (userNumber > randomNumber) {
+//                System.out.println("Too big... guess again...");
+//            } else if (userNumber < randomNumber) {
+//                System.out.println("Too small... guess again...");
+//            } else {
+//                System.out.println("Great Guess! Well done!");
+//                break;
+//            }
+//        }
+//    }
+//}
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        int userNumber;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hey! Enter a number between 1-100");
+        int A = scanner.nextInt();
+        System.out.println(A);
 
-        int randomNumber = (int) (Math.random() * 100) + 1; // Generating random number outside the loop
-
-        while (true) {
-            userNumber = scanner.nextInt();
-            if (userNumber < 1 || userNumber > 100) {
-                System.out.println("** Please try again **");
-            } else if (userNumber > randomNumber) {
-                System.out.println("Too big... guess again...");
-            } else if (userNumber < randomNumber) {
-                System.out.println("Too small... guess again...");
-            } else {
-                System.out.println("Great Guess! Well done!");
-                break;
-            }
-        }
     }
 }
